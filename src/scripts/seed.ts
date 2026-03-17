97d251d9c293063c9a8354deb9375b6302a77b84
dSumitabha <fullstackdev@zanservices.com>
2026-03-17 16:19:08 +0530

    client seeder with project seeder added

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -1,12 +1,16 @@
 import "dotenv/config"
 
 import seedLeads from "@/seeder/leadSeeder"
+import seedClients from "@/seeder/clientSeeder"
+import seedProjects from "@/seeder/projectSeeder"
 
 async function runSeed() {
     try {
         console.log("Seeding Started...")
 
         await seedLeads()
+        await seedClients()
+        await seedProjects()
 
         console.log("Seeding Completed")
         process.exit(0)
